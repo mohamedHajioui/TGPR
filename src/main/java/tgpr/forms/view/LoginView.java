322e600e7b0e7951f474dd7851a4f12d7de0b492
Hamza Stitou <hamzastitou11s@hotmail.com>
2024-10-08 20:11:10 +0200

    feat(login) : creation du bouton LoginAsGuest

--- src/main/java/tgpr/forms/view/LoginView.java
+++ src/main/java/tgpr/forms/view/LoginView.java
@@ -9,9 +9,13 @@ import java.util.List;
 public class LoginView extends BasicWindow {
 
     private final LoginController controller;
-    private final TextBox txtPseudo;
+    private final TextBox txtMail;
     private final TextBox txtPassword;
     private final Button btnLogin;
+
+    private final Button btnExit;
+    private final Button btnLoginAsGuest;
+
 
     public LoginView(LoginController controller) {
         this.controller = controller;
@@ -25,7 +29,7 @@ public class LoginView extends BasicWindow {
         Panel panel = new Panel().setLayoutManager(new GridLayout(2).setTopMarginSize(1).setVerticalSpacing(1))
                 .setLayoutData(Layouts.LINEAR_CENTER).addTo(root);
         panel.addComponent(new Label("Mail :"));
-        txtPseudo = new TextBox().addTo(panel);
+        txtMail = new TextBox().addTo(panel);
         panel.addComponent(new Label("Password:"));
         txtPassword = new TextBox().setMask('*').addTo(panel);
 
@@ -34,21 +38,20 @@ public class LoginView extends BasicWindow {
         Panel buttons = new Panel().setLayoutManager(new LinearLayout(Direction.HORIZONTAL))
                 .setLayoutData(Layouts.LINEAR_CENTER).addTo(root);
         btnLogin = new Button("Login", this::login).addTo(buttons);
-        //btnSignup = new Button("signup", this::login).addTo(buttons);
-        Button btnExit = new Button("Exit", this::exit).addTo(buttons);
-        //btnLoginAsGuest = new Button("Login as Guest", this::login).addTo(buttons);
+        //btnSignup = new Button("signup", this::signup).addTo(buttons);
+        btnExit = new Button("Exit", this::exit).addTo(buttons);
+        btnLoginAsGuest = new Button("Login as Guest", this::loginasguest).addTo(buttons);
 
         new EmptySpace().addTo(root);
 
         Button btnSeedData = new Button("Reset Database", this::seedData);
         Panel debug = Panel.verticalPanel(LinearLayout.Alignment.Center,
                 new Button("Login as default admin", this::logAsDefaultAdmin),
-                new Button("Login as default member", this::logAsDefaultMember),
                 btnSeedData
         );
         debug.withBorder(Borders.singleLine(" For debug purpose ")).addTo(root);
 
-        txtPseudo.takeFocus();
+        txtMail.takeFocus();
     }
 
     private void seedData() {
@@ -61,9 +64,9 @@ public class LoginView extends BasicWindow {
     }
 
     private void login() {
-        var errors = controller.login(txtPseudo.getText(), txtPassword.getText());
+        var errors = controller.login(txtMail.getText(), txtPassword.getText());
         if (!errors.isEmpty()) {
-            txtPseudo.takeFocus();
+            txtMail.takeFocus();
         }
     }
 
@@ -71,7 +74,7 @@ public class LoginView extends BasicWindow {
         controller.login(Configuration.get("default.admin.pseudo"), Configuration.get("default.admin.password"));
     }
 
-    private void logAsDefaultMember() {
-        controller.login(Configuration.get("default.member.pseudo"), Configuration.get("default.member.password"));
+    private void loginasguest() {
+        controller.login(Configuration.get("guest.fullname"), Configuration.get("guest.password"));
     }
 }
