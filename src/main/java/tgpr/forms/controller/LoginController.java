957fd390fda5d5c5dc40dd7c6aa797fb0fc72383
Hamza Stitou <hamzastitou11s@hotmail.com>
2024-10-08 16:19:08 +0200

    feat login : Controller

--- src/main/java/tgpr/forms/controller/LoginController.java
+++ src/main/java/tgpr/forms/controller/LoginController.java
@@ -1,4 +1,41 @@
 package tgpr.forms.controller;
 
-public class LoginController {
+import tgpr.forms.FormsApp;
+import tgpr.forms.model.Security;
+import tgpr.forms.view.LoginView;
+import tgpr.framework.Controller;
+import tgpr.framework.ErrorList;
+import tgpr.framework.Model;
+
+public class LoginController extends Controller<LoginView> {
+    public void exit() {
+        System.exit(0);
+    }
+
+    public List<Error> login(String pseudo, String password) {
+        var errors = new ErrorList();
+        errors.add(MemberValidator.isValidPseudo(pseudo));
+        errors.add(MemberValidator.isValidPassword(password));
+
+        if (errors.isEmpty()) {
+            var member = Member.checkCredentials(pseudo, password);
+            if (member != null) {
+                Security.login(member);
+                navigateTo(new MemberListController());
+            } else
+                showError(new Error("invalid credentials"));
+        } else
+            showErrors(errors);
+
+        return errors;
+    }
+
+    public void seedData() {
+        Model.seedData(FormsApp.DATABASE_SCRIPT_FILE);
+    }
+
+    @Override
+    public LoginView getView() {
+        return new LoginView(this);
+    }
 }
