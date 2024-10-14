d6311b7e475d8767a12b94d758eda8c74cab9dd3
Aly Samir <2208alsamirmahmoudib@student.epfc.eu>
2024-10-14 11:05:43 +0200

    feat(view_form): introduction de la class Form dans le controller et la suppression de la class  User precedament introduit

--- src/main/java/tgpr/forms/controller/formController.java
+++ src/main/java/tgpr/forms/controller/formController.java
@@ -1,5 +1,6 @@
 package tgpr.forms.controller;
 
+import tgpr.forms.model.Form;
 import tgpr.framework.Controller;
 import tgpr.framework.ErrorList;
 import tgpr.forms.view.view_form;
@@ -7,15 +8,15 @@ import tgpr.forms.model.User;
 
 import java.lang.reflect.Member;
 
-public class formController extends Controller {
+public class formController extends Controller<view_form> {
 
-    private final User user;
+    private final Form form;
 
-    public formController(User user) {
-        this.user = user;
+    public formController(Form form) {
+        this.form = form;
     }
 
-    public view_form getView() {return new view_form(this);}
+    public view_form getView() {return new view_form(this,form);}
 
 
 
