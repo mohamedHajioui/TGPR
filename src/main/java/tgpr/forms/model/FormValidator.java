14fdfea5eb0d3c98a0e685d7a7cea3d910c6b2ed
Cihan Aslan <1212ciaslan@student.epfc.eu>
2024-10-15 23:57:14 +0200

    feat(add_edit_form): implémentation du modèle FormValidator

---
+++ src/main/java/tgpr/forms/model/FormValidator.java
@@ -0,0 +1,15 @@
+package tgpr.forms.model;
+
+public class FormValidator {
+
+    public static String isValidAvailableTitle(String title) {
+        if (title == null || title.isBlank()) {
+            return "title required";
+        }
+        if (title.length() < 3) {
+            return "min 3 characters required";
+        }
+        return null;
+    }
+
+}
