ae80a4c0b1de15cdb03605f1e53401ff2745164b
Mohamed El Hajioui <mohamedelhajioui3@gmail.com>
2024-10-22 20:03:07 +0200

    feat(signup): affichage des pessages d'erreur

--- src/main/java/tgpr/forms/controller/SignupController.java
+++ src/main/java/tgpr/forms/controller/SignupController.java
@@ -3,8 +3,14 @@ package tgpr.forms.controller;
 import tgpr.forms.view.SignupView;
 import tgpr.framework.Controller;
 
+import java.util.regex.Pattern;
+
 public class SignupController extends Controller<SignupView> {
     private final SignupView view;
+    private static final Pattern EMAIL_PATTERN =
+            Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).{8,}$");
 
 
     public SignupController() {
@@ -19,17 +25,48 @@ public class SignupController extends Controller<SignupView> {
         System.exit(0);
     }
 
-    public boolean isValidEmail(String email) {
-        return email != null && email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
-    }
-
-    public void onMailChanged(String email) {
-        if (!isValidEmail(email)) {
-            view.setMailErrorMessage("Not a valid email");
+    public void isValidEmail(String email) {
+        if (email.isBlank()){
+            view.setMailErrorMessage("");
+        } else if (!EMAIL_PATTERN.matcher(email).matches()) {
+            view.setMailErrorMessage("                    Invalid mail");
         } else {
-            view.setMailErrorMessage(null);  // Efface le message d'erreur si l'email est valide
+            view.setMailErrorMessage("");
         }
     }
+
+
+    public void isValidName(String name) {
+        if (name.isBlank()){
+            view.setNameErrorMessage("");
+        } else if (name.length() < 3) {
+            view.setNameErrorMessage("                    Minimum 3 chars");
+        } else {
+            view.setNameErrorMessage("");
+        }
+    }
+
+    public void isValidPassword(String password) {
+        if (password.isEmpty()){
+            view.setPasswordErrorMessage("");
+        } else {
+            if (password.length() < 8) {
+                view.setPasswordErrorMessage("                    Minimum 8 chars");
+            } else if (!password.matches(".*\\d.*")) {
+                view.setPasswordErrorMessage("                    Minimum 1 digit.");
+            } else if (!password.matches(".*[A-Z].*")) {
+                view.setPasswordErrorMessage("                    Minimum 1 uppercase letter.");
+            } else if (!password.matches(".*[^a-zA-Z0-9].*")) {
+                view.setPasswordErrorMessage("                    Minimum 1 special character.");
+            } else {
+                view.setPasswordErrorMessage("");  // Mot de passe valide
+            }
+        }
+    }
+
+
+
+
 }
 
 
--- src/main/java/tgpr/forms/view/SignupView.java
+++ src/main/java/tgpr/forms/view/SignupView.java
@@ -2,18 +2,18 @@ package tgpr.forms.view;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.*;
-import tgpr.forms.controller.LoginController;
 import tgpr.forms.controller.SignupController;
 import com.googlecode.lanterna.gui2.dialogs.DialogWindow;
-import tgpr.forms.controller.SignupController;
-import tgpr.framework.Controller;
 
 import java.util.List;
 
 public class SignupView extends DialogWindow {
     private final SignupController controller;
     private Panel mainPanel;
-    private final Label errorLabel;
+    private Label errorLabelMail;
+    private Label errorLabelName;
+    private Label errorLabelPassword;
+    private Label hashedPasswordLabel;
 
 
     public SignupView(SignupController controller) {
@@ -27,28 +27,54 @@ public class SignupView extends DialogWindow {
         setComponent(mainPanel);
         mainPanel.addComponent(new EmptySpace(new TerminalSize(1, 1)));
 
+
+
         //creation du formulaire a remplir
         mail(controller);
-        errorLabel = new Label("").setForegroundColor(TextColor.ANSI.RED);
-        errorLabel.setPreferredSize(new TerminalSize(40, 0));
-        mainPanel.addComponent(errorLabel);
         fullName();
         password();
         confirmPassword();
-
         //ajout des boutons signup et close
         buttonsSignupAndClose();
     }
 
     private void mail(SignupController controller) {
+        errorLabelMail = new Label("").setForegroundColor(TextColor.ANSI.RED);
+        errorLabelMail.setPreferredSize(new TerminalSize(40, 1));
         Panel mailPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
         Label mailLabel = new Label(" Mail:             ");
         mailPanel.addComponent(mailLabel);
         TextBox mailBox = new TextBox(new TerminalSize(22, 1));
         mailPanel.addComponent(mailBox);
         mainPanel.addComponent(mailPanel);
-        mailBox.setTextChangeListener((newText, changedByUser) -> controller.onMailChanged(newText));
-        mainPanel.addComponent(new EmptySpace(new TerminalSize(1, 1)));
+        mailBox.setTextChangeListener((newText, changedByUser) -> controller.isValidEmail(newText));
+        mainPanel.addComponent(errorLabelMail);
+    }
+
+    private void fullName() {
+        errorLabelName = new Label("").setForegroundColor(TextColor.ANSI.RED);
+        errorLabelName.setPreferredSize(new TerminalSize(40, 1));
+        Panel fullNamePanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+        Label fullNameLabel = new Label(" Full Name:        ");
+        fullNamePanel.addComponent(fullNameLabel);
+        TextBox fullNameBox = new TextBox(new TerminalSize(40, 1));
+        fullNamePanel.addComponent(fullNameBox);
+        mainPanel.addComponent(fullNamePanel);
+        fullNameBox.setTextChangeListener((newText, changedByUser) -> controller.isValidName(newText));
+        mainPanel.addComponent(errorLabelName);
+    }
+
+    private void password() {
+        errorLabelPassword = new Label("").setForegroundColor(TextColor.ANSI.RED);
+        errorLabelPassword.setPreferredSize(new TerminalSize(40, 1));
+        Panel passwordPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+        Label passwordLabel = new Label(" Password:         ");
+        passwordPanel.addComponent(passwordLabel);
+        TextBox passwordBox = new TextBox(new TerminalSize(22, 1));
+        passwordPanel.addComponent(passwordBox);
+        mainPanel.addComponent(passwordPanel);
+        passwordBox.setTextChangeListener((newText, changedByUser) -> controller.isValidPassword(newText));
+        mainPanel.addComponent(errorLabelPassword);
     }
 
     private void buttonsSignupAndClose() {
@@ -68,35 +94,26 @@ public class SignupView extends DialogWindow {
         TextBox confirmPasswordBox = new TextBox(new TerminalSize(22, 1));
         confirmPasswordPanel.addComponent(confirmPasswordBox);
         mainPanel.addComponent(confirmPasswordPanel);
-        mainPanel.addComponent(new EmptySpace(new TerminalSize(3, 3)));
+        mainPanel.addComponent(new EmptySpace(new TerminalSize(1, 10)));
     }
 
-    private void password() {
-        Panel passwordPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
-        Label passwordLabel = new Label(" Password:         ");
-        passwordPanel.addComponent(passwordLabel);
-        TextBox passwordBox = new TextBox(new TerminalSize(22, 1));
-        passwordPanel.addComponent(passwordBox);
-        mainPanel.addComponent(passwordPanel);
-        mainPanel.addComponent(new EmptySpace(new TerminalSize(1, 1)));
-    }
 
-    private void fullName() {
-        Panel fullNamePanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
-        Label fullNameLabel = new Label(" Full Name:        ");
-        fullNamePanel.addComponent(fullNameLabel);
-        TextBox fullNameBox = new TextBox(new TerminalSize(40, 1));
-        fullNamePanel.addComponent(fullNameBox);
-        mainPanel.addComponent(fullNamePanel);
-        mainPanel.addComponent(new EmptySpace(new TerminalSize(1, 1)));
-    }
+
 
     public void setMailErrorMessage(String message) {
-        errorLabel.setText(message != null ? message : "");
+        errorLabelMail.setText(message != null ? message : "");
+    }
+
+    public void setNameErrorMessage(String message) {
+        errorLabelName.setText(message != null ? message : "");
+    }
+
+    public void setPasswordErrorMessage(String password){
+        errorLabelPassword.setText(password != null ? password : "");
     }
 
 
-
-
-
+    public void displayHashedPassword(String hashedPassword) {
+        hashedPasswordLabel.setText("Hashed Password: " + hashedPassword);
+    }
 }
