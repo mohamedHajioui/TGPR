3d73d58dfe1f24610818b746e16cfd4621b1e2a7
Amine <0504amihdene@student.epfc.eu>
2024-10-27 00:51:24 +0200

    feat(view_edit_instance):  creation de la fenetre SubmitForm pour confirmer la submission ainsi que ces buttons

--- src/main/java/tgpr/forms/view/EditInstanceView.java
+++ src/main/java/tgpr/forms/view/EditInstanceView.java
@@ -591,6 +591,45 @@ public class EditInstanceView extends DialogWindow {
                 // Actualiser l'affichage du panneau des boutons
                 buttonPanel.invalidate(); // Rafraîchir l'affichage des boutons
             }
+            private void ConfirmationOfSubmission() {
+                setViewTitle("Submit form");
+                mainPanel = new Panel(new LinearLayout(Direction.VERTICAL));
+                mainPanel.setPreferredSize(new TerminalSize(55, 5)); // Définir une taille préférée pour le panel
+
+                Label textLabel = new Label("Are you sure you want to submit this form ?");
+                mainPanel.addComponent(textLabel); // Ajouter le label au panel principal
+
+
+                mainPanel.addComponent(new EmptySpace(new TerminalSize(0, 1)), LinearLayout.createLayoutData(LinearLayout.Alignment.Fill));
+
+
+                Panel buttonPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+                buttonPanel.addComponent(new Button("Yes", this::ButtonConfirmYes));
+                buttonPanel.addComponent(new Button("No", this::ButtonConfirmNo));
+
+                setHints(List.of(Hint.CENTERED));
+
+                buttonPanel.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+
+                mainPanel.addComponent(buttonPanel);
+
+                Panel container = new Panel(new LinearLayout(Direction.HORIZONTAL));
+                container.setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.Center));
+                container.addComponent(new EmptySpace(new TerminalSize(0, 1))); // Espace vide avant
+                container.addComponent(mainPanel);
+                container.addComponent(new EmptySpace(new TerminalSize(0, 1))); // Espace vide après
+
+                // Définir le panneau de conteneur comme composant principal de la fenêtre
+                setComponent(container);
+
+            }
+            private void ButtonConfirmYes() {
+                displayAnswerList();
+                IndicateFormSubmitted();
+            }
+            private void ButtonConfirmNo() {
+
+            }
 
 
 
