b31fc0afe437c9c654dee1c3cc0bd21996421f7a
Cihan Aslan <1212ciaslan@student.epfc.eu>
2024-11-04 02:19:30 +0100

    feat(add_edit_option_list): insertion boutons et méthodes reloadData et addOptionValue

--- src/main/java/tgpr/forms/view/AddEditOptionListView.java
+++ src/main/java/tgpr/forms/view/AddEditOptionListView.java
@@ -8,6 +8,7 @@ import com.googlecode.lanterna.gui2.GridLayout;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
 import com.googlecode.lanterna.gui2.dialogs.DialogWindow;
+import com.googlecode.lanterna.input.KeyType;
 import tgpr.forms.controller.AddEditOptionListController;
 import tgpr.forms.model.Form;
 import tgpr.forms.model.OptionList;
@@ -15,6 +16,7 @@ import tgpr.forms.model.OptionValue;
 import tgpr.forms.model.User;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -28,7 +30,6 @@ public class AddEditOptionListView extends DialogWindow {
     private final Panel namePanel;
     private TextBox txtName;
     private final Label errName;
-    private final Panel tablePanel;
     private final ObjectTable<OptionValue> table;
     private final Panel addOptionPanel;
     private final Label errAddOption;
@@ -36,9 +37,13 @@ public class AddEditOptionListView extends DialogWindow {
     private Button btnAddOption;
     private final Panel btnPanel;
     private final Panel btnContainer;
+    private final Button btnReorder;
+    private final Button btnDuplicate;
+    private final Button btnAlpha;
+    private final Button btnConfirm;
+//    private final Button btnCancel;
     private Button btnCreate;
     private final Button btnClose;
-
     private List<OptionValue> options = new ArrayList<>();
 
     public AddEditOptionListView(AddEditOptionListController controller, User owner, OptionList optionList) {
@@ -52,7 +57,8 @@ public class AddEditOptionListView extends DialogWindow {
 //        setCloseWindowWithEscape(true);
         setFixedSize(new TerminalSize(50, 15));
 
-        root = new Panel().setLayoutManager(new LinearLayout(Direction.VERTICAL));
+        root = new Panel().setLayoutManager(new GridLayout(1));
+        setComponent(root);
 
         namePanel = new Panel().addTo(root)//.setLayoutData()
                 .setLayoutManager(new GridLayout(2)
@@ -64,16 +70,21 @@ public class AddEditOptionListView extends DialogWindow {
         new EmptySpace().addTo(namePanel);
         errName = new Label("name required").addTo(namePanel).setForegroundColor(TextColor.ANSI.RED);
 
-        tablePanel = new Panel().addTo(root)
-                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.FILL, GridLayout.Alignment.FILL, true, false))
-                .setLayoutManager(new LinearLayout(Direction.VERTICAL));
         table = new ObjectTable<>(
                 new ColumnSpec<>("Index", optionValue -> options.indexOf(optionValue) + 1),
                 new ColumnSpec<>("Label", OptionValue::getLabel)
         );
-        //        root.addComponent(table);
-        table.setPreferredSize(new TerminalSize(getTerminalColumns(), 10));
-        tablePanel.addComponent(table);
+        root.addComponent(table);
+        table.setPreferredSize(new TerminalSize(getTerminalColumns(), 8));
+        table.setKeyStrokeHandler(keyStroke -> {
+            if (keyStroke.getKeyType() == KeyType.Delete) {
+                OptionValue selectedOption = table.getSelected();
+                if (selectedOption != null) {
+                    controller.optionValueDelete(selectedOption);
+                }
+            }
+        });
+        new EmptySpace().addTo(root);
 
         addOptionPanel = new Panel().addTo(root)
                 .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.FILL, GridLayout.Alignment.BEGINNING, true, false))
@@ -91,90 +102,94 @@ public class AddEditOptionListView extends DialogWindow {
                     btnCreate.setEnabled(controller.canCreateOptionList());
                 });
 
-        btnAddOption = new Button("Add", this::addOption).addTo(addOptionPanel)
+        btnAddOption = new Button("Add", this::addOptionValue).addTo(addOptionPanel)
                 .setLayoutData(LinearLayout.createLayoutData(LinearLayout.Alignment.End));
 
+        new EmptySpace().addTo(root);
+
         btnPanel = new Panel().addTo(root)
-                .setLayoutData(BorderLayout.Location.BOTTOM)
+                .setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.END, GridLayout.Alignment.BEGINNING))
                 .setLayoutManager(new LinearLayout(Direction.HORIZONTAL));
 
-        new EmptySpace().addTo(btnPanel);
 
-        btnContainer = new Panel(new LinearLayout(Direction.HORIZONTAL));
+        btnContainer = new Panel(new LinearLayout(Direction.HORIZONTAL)).addTo(btnPanel);
+
+        btnReorder = new Button("Reorder", this::reorder).addTo(btnContainer);
+
+        btnDuplicate = new Button("Duplicate", this::duplicate).addTo(btnContainer);
+
+        btnAlpha = new Button("Alphabetically", this::alphabetically).addTo(btnContainer);
+
+        btnConfirm = new Button("Confirm", this::confirmOrder).addTo(btnContainer);
+
+//        btnCancel = new Button("Cancel", this::cancelOrder).addTo(btnContainer);
+
         btnCreate = new Button(optionList == null ? "Create" : "Save", this::createOrUpdateOptionList)
                 .addTo(btnContainer).setEnabled(false);
-        btnClose = new Button("Close", this::cancelCreation).addTo(btnContainer);
 
-        btnContainer.addTo(btnPanel);
-
-        setComponent(root);
+        btnClose = new Button("Close", this::close).addTo(btnContainer);
 
         if (optionList != null) {
             txtName.setText(optionList.getName());
+            reloadData();
         }
     }
 
-    private void addOption() {
-        String optionLabel = txtAddOption.getText().trim();
-        if (optionLabel.isEmpty()) {
-            errAddOption.setVisible(true);
-            btnAddOption.setEnabled(false);
-        } else {
-            OptionValue newOption = new OptionValue(optionList, options.size(), optionLabel);
-            boolean added = controller.addOption(newOption);
-            if (added) {
-                errAddOption.setVisible(false);
-                txtAddOption.setText("");
-                table.setItems(controller.getOptions());
-                btnCreate.setEnabled(controller.canCreateOptionList());
-            } else {
-                errAddOption.setVisible(true);
-            }
-        }
+    private void reorder() {
+        controller.reorder();
     }
+
+    private void duplicate() {
+        controller.duplicate();
+    }
+
+    private void alphabetically() {
+        controller.alphabetically();
+    }
+    private void confirmOrder() {
+        controller.confirmOrder();
+    }
+/*
+    private void cancelOrder() {
+        controller.cancelOrder(options);   // !!!
+    }
+*/
 
     private void createOrUpdateOptionList() {
         if (optionList == null) {
-            controller.addOptionList(txtName.getText());
-            optionList.setOwnerId(owner.getId());
+            optionList = controller.createOptionList(txtName.getText());
         } else {
-            optionList.setName(txtName.getText());
-            controller.updateOptionList(txtName.getText());
-            close();
-            return;
+            controller.updateOptionList(optionList);
         }
-        addOptionList(optionList);
-    }
-
-    private void addOptionList(OptionList optionList) {
-        var errors = controller.validate(optionList.getName());
-        if (errors.isEmpty()) {
-            optionList.save();
-            for (OptionValue optionValue : options) {
-                optionValue.setOptionListId(optionList.getId());
-                optionValue.save();
-            }
-            close();
-        }
-    }
-
-    private void cancelCreation() {
-        close();
+        controller.saveOptionValues();
     }
 
     private void validate() {
         var errors = controller.validate(txtName.getText());
-
         boolean hasOptions = !options.isEmpty();
-
         if (!hasOptions) {
             errAddOption.setVisible(true);
             btnCreate.setEnabled(false);
         } else {
             errAddOption.setVisible(false);
         }
-
         errName.setText(errors.getFirstErrorMessage(Form.Fields.Name));
         btnCreate.setEnabled(errors.isEmpty() && hasOptions);
     }
+
+    public void reloadData() {
+        options.clear();
+        options.addAll(optionList.getOptionValues());
+        options.sort(Comparator.comparingInt(OptionValue::getIdx));
+        table.clear();
+        table.add(options);
+    }
+
+    private void addOptionValue() {
+        String label = txtAddOption.getText();
+        controller.addOptionValue(label);
+        txtAddOption.setText("");
+    }
+
+
 }
